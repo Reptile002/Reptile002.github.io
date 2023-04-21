0c90d12376c2b9e34fbd5edeff734fb9f0ee0f47
Reptile002 <szymonolechno9@gmail.com>
2023-04-21 15:13:41 +0200

    update

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
 
 
 const howMuchToPay = (event) => {
+
     event.preventDefault()
     let fuelCost = document.getElementById(`fuelCost`).value.replace(`,`,`.`)
     let fuelConsumption = document.getElementById(`consumption`).value.replace(`,`,`.`)
     let distance = document.getElementById(`distance`).value.replace(`,`,`.`)
-    let peopleInCar = document.getElementById(`pplInCar`).value.replace(`,`,`.`)
-    let result = null
-    console.log(fuelCost, fuelConsumption, distance, peopleInCar)
+    let peopleInCar = document.getElementById(`pplInCar`).value
+    let result
 
-    if(peopleInCar == 0){
-        result = document.getElementById(`result`).innerHTML =`
-        <p class="errorTxt"><br>
-        Wprowadzone dane są nieprawidłowe
-        </p>`
+    if(peopleInCar == 0 || fuelCost == 0 || fuelConsumption == 0 || distance == 0){
+
+        result = document.getElementById(`err`).innerHTML =`<p class="errorTxt"><br>Wprowadzone dane są nieprawidłowe</p>`,
+        result = document.getElementById(`result`).innerHTML =``
+
+    }else if(peopleInCar == 1){
+
+        let usedFuel = fuelConsumption / 100 * distance
+        let cost = [usedFuel * fuelCost, usedFuel * fuelCost / peopleInCar]
+
+        result = document.getElementById(`result`).innerHTML =`<p class="costTxt">Łączny koszt paliwa : <p class="thiccTxt">${cost[0].toFixed(2)} PLN</p>`,
+        result = document.getElementById(`err`).innerHTML =``
+
     }else{
-    let usedFuel = fuelConsumption / 100 * distance
-    let cost = [usedFuel * fuelCost, usedFuel * fuelCost / peopleInCar]
-    result = document.getElementById(`result`).innerHTML =`
-    <p class="costTxt"><br>
-        Łączny koszt paliwa : <p class="thiccTxt">${cost[0].toFixed(2)} PLN</p><hr class ="line"> <p class ="costTxt">Cena za osobę </p> <p class="thiccTxt">${cost[1].toFixed(2)} PLN</p>
-    </p>`}
+
+        let usedFuel = fuelConsumption / 100 * distance
+        let cost = [usedFuel * fuelCost, usedFuel * fuelCost / peopleInCar]
+
+        result = document.getElementById(`result`).innerHTML =`
+        <p class="costTxt">Łączny koszt paliwa : <p class="thiccTxt">${cost[0].toFixed(2)} PLN</p><hr class ="line"> <p class ="costTxt">Cena za osobę </p> <p class="thiccTxt">${cost[1].toFixed(2)} PLN</p></p>`,
+        result = document.getElementById(`err`).innerHTML =``
+
+    }
 
     return result
 }
